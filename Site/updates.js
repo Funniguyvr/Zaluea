aca9848287f5736b416ebe083d3af10123c830fa
Funniguyvr <163761474+Funniguyvr@users.noreply.github.com>
2026-08-07 17:11:35 -0400

    updates.js

--- Site/updates.js
+++ Site/updates.js
@@ -1,5 +1,5 @@
 var updates = [
-    {message: "Welcome to the 3rd official release of Andyum!"}
+    {message: "Kinda Yoinked this but this is Funni's Google Classroom (shh) "}
 ];
 var updatespage = document.getElementById("updatespage");
 for(let item of updates) {
